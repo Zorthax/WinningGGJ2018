97f7337659e3f4755f8004ac4c42171c7518f423
AIE\Student <casslindsay01@gmail.com>
2018-01-27 08:02:30 +1030

    -Fish fin joints and floppiness

---
+++ Assets/Scripts/FishFlop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishFlop : MonoBehaviour
+{
+    public List<HingeJoint> jointList;
+    float intervalTime = 5;
+    float timer = 4.5f;
+    float buffer = 0.5f;
+
+
+	void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= intervalTime)
+        {
+            foreach (HingeJoint joint in jointList)
+            {
+                JointMotor motor = joint.motor;
+                joint.useMotor = true;
+
+                if (timer >= intervalTime + buffer)
+                    timer = 0;
+            }
+        }
+
+        else
+            foreach (HingeJoint joint in jointList)
+            {
+                JointMotor motor = joint.motor;
+                joint.useMotor = false;
+            }
+    }
+}
