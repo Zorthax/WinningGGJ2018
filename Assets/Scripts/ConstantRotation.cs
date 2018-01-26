1459d430fdd432d74503a8d8df02188b0decb596
AIE\adam.schmidt <ad.schmidt7>
2018-01-26 23:39:00 +1030

    We have a fish! And a level And a shitty alarm clock

---
+++ Assets/Scripts/ConstantRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotation : MonoBehaviour {
+
+    [SerializeField]
+    Vector3 rotationSpeed;
+
+    Quaternion startRotation;
+
+	// Use this for initialization
+	void Start ()
+    {
+        startRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+	}
+
+    public void ResetRotation()
+    {
+        //transform.rotation = startRotation;
+    }
+}
