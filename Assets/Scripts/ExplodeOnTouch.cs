cfc0ab948366a3c07186ccc89cb22121c65f9c00
AIE\adam.schmidt <ad.schmidt7>
2018-01-28 08:15:36 +1030

    level 4 and 5

---
+++ Assets/Scripts/ExplodeOnTouch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeOnTouch : MonoBehaviour {
+
+    public GameObject whatToTurnOn;
+    public GameObject whatToTurnOff;
+    public bool forceBasedOnPosition;
+    public Vector2 explosionForce = new Vector3(-300, 200);
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            whatToTurnOn.SetActive(true);
+            if (!forceBasedOnPosition)
+                other.transform.GetComponentInChildren<Rigidbody2D>().AddForce(explosionForce);
+            else
+                other.transform.GetComponentInChildren<Rigidbody2D>().AddForce((other.transform.position - transform.position) * 30);
+            whatToTurnOff.SetActive(false);
+        }
+    }
+
+}
---
+++ Assets/Scripts/ReplaceOnPause.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceOnPause : MonoBehaviour {
+
+    public GameObject objectToReplaceSelf;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (PlaymodeManager.mode != PlaymodeManager.Mode.playing)
+        {
+            objectToReplaceSelf.SetActive(true);
+            gameObject.SetActive(false);
+        }
+	}
+}
