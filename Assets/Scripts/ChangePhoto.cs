7fbaa9c873bc67feaf0bd73f021cbf485634da3c
AIE\adam.schmidt <ad.schmidt7>
2018-01-28 15:05:38 +1030

    Game complete (and buggy)

---
+++ Assets/Scripts/ChangePhoto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePhoto : MonoBehaviour {
+
+    public GameObject imageToTurnOff;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(1))
+        {
+            imageToTurnOff.SetActive(!imageToTurnOff.activeSelf);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("level_0");
+        }
+    }
+}
